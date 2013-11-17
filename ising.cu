239e61c873fa1be11656622c3374cf07302f5fe9
Dylan Howey <howeyman09@gmail.com>
2013-11-16 19:52:30 -0600

    added neighbors. should be doulbe checked just to be sure

--- ising.cu
+++ ising.cu
@@ -7,7 +7,7 @@
 //! @param T The temperature, in units of epsilon/k. Epsilon is the exchange energy and k is the boltzmann constant
 __global__ void ising(int * lattice, int height, int width, float T) {
 	__shared__ int slattice[BLOCK_SIZE][BLOCK_SIZE];
-	__constant__ int neighbors[4 * BLOCK_SIZE];
+	__shared__ int sneighbors[4 * BLOCK_SIZE];
 
 	int tx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
 	int ty = blockIdx.y * BLOCK_SIZE + threadIdx.y;
@@ -16,16 +16,33 @@ __global__ void ising(int * lattice, int height, int width, float T) {
 	//Load sublattice into shared memory
 	if(tx < width && ty < height) {
 		slattice[threadIdx.y][threadIdx.x] = lattice[ty * width + threadIdx.x];
-		//TODO: Figure out how to use constant memory
-		//Maybe put the entire lattice into constant memory?
-		#if 0
 		if(threadIdx.y == 0) {
 			if(ty == 0)
-				neighbors[threadIdx.x] = lattice[(height - 1) * width + tx];
+				sneighbors[threadIdx.x] = lattice[(height - 1) * width + tx];
 			else
-				neighbors[threadIdx.x] = lattice[(ty - 1) * width + tx];
+				sneighbors[threadIdx.x] = lattice[(ty - 1) * width + tx];
 		}
-		#endif
+
+		if(threadIdx.y == (BLOCK_SIZE - 1)) {
+			if(ty == (height - 1))
+				sneighbors[2 * BLOCK_SIZE + threadIdx.x] = lattice[tx];
+			else
+				sneighbors[2 * BLOCK_SIZE + threadIdx.x] = lattice[(ty + 1) * width + tx];
+		}
+
+		if(threadIdx.x == 0) {
+			if(tx == 0)
+				sneighbors[3 * BLOCK_SIZE + threadIdx.y] = lattice[ty * width + (width - 1)];
+			else
+				sneighbors[3 * BLOCK_SIZE + threadIdx.y] = lattice[ty * width + (tx - 1)];
+		}
+
+		if(threadIdx.x == (BLOCK_SIZE - 1)) {
+			if(tx == (width - 1))
+				sneighbors[BLOCK_SIZE + threadIdx.y] = lattice[ty * width];
+			else
+				sneighbors[BLOCK_SIZE + threadIdx.y] = lattice[ty * width + (tx + 1)];
+		}
 		__syncthreads();
 
 	//Perform simulation
@@ -33,26 +50,24 @@ __global__ void ising(int * lattice, int height, int width, float T) {
 		//Checkerboard
 		if((threadIdx.x + threadIdx.y) % 2 == i) {
 			if(threadIdx.y == 0)
-				top =  
+				top = sneighbors[threadIdx.x]; 
 			else
 				top = slattice[threadIdx.y - 1][threadIdx.x];
 			
 			if(threadIdx.x == 0)
-				left = 
+				left = sneighbors[3 * BLOCK_SIZE + threadIdx.y];
 			else
 				left = slattice[threadIdx.y][threadIdx.x - 1];
 
 			if(threadIdx.y == (BLOCK_SIZE - 1))
-				bottom =
+				bottom = sneighbors[2 * BLOCK_SIZE + threadIdx.x]
 			else
 				bottom = slattice[threadIdx.y + 1][threadIdx.x];
 
 			if(threadIdx.x == (BLOCK_SIZE - 1))
-				right =
+				right = sneighbors[BLOCK_SIZE + threadIdx.y]; 
 			else
 				right = slattice[threadIdx.y][threadIdx.x + 1];
-
-			__syncthreads(); //Is this needed?
 
 			//Calculate change in energy if dipole were flipped
 			deltaU = 2 * slattice[threadIdx.y][threadIdx.x] * (top + bottom + left + right);
@@ -67,8 +82,10 @@ __global__ void ising(int * lattice, int height, int width, float T) {
 				if(rand < powf(2.71f, -deltaU/T))
 					slattice[threadIdx.y][threadIdx.x] *= -1;
 			}
+			__syncthreads(); //Is this needed?
 		}
 	}
+	slattice[threadIdx.y][threadIdx.x] = lattice[ty * width + threadIdx.x];
 	}
 
 }
