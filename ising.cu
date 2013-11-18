d7219a9d510a695602932f29815f368b0acb9cc4
Dylan Howey <howey014@umn.edu>
2013-11-18 10:42:05 -0600

    added iterations

--- ising.cu
+++ ising.cu
@@ -4,8 +4,9 @@
 //! @param lattice A pointer to the lattice of atoms.
 //! @param height The number of rows of the input lattice.
 //! @param width The number of columns of the input lattice.
-//! @param T The temperature, in units of epsilon/k. Epsilon is the exchange energy and k is the boltzmann constant
-__global__ void ising(int * lattice, int height, int width, float T) {
+//! @param T The temperature, in units of epsilon/k. Epsilon is the exchange energy and k is the boltzmann constant.
+//! #param iterations The number of Metropolis iterations to perform.
+__global__ void ising(int * lattice, int height, int width, float T, unsigned int iterations) {
 	__shared__ int slattice[BLOCK_SIZE][BLOCK_SIZE];
 	__shared__ int sneighbors[4 * BLOCK_SIZE];
 
@@ -45,46 +46,49 @@ __global__ void ising(int * lattice, int height, int width, float T) {
 		}
 		__syncthreads();
 
-	//Perform simulation
-	for(int i = 0; i < 2; i ++) {
-		//Checkerboard
-		if((threadIdx.x + threadIdx.y) % 2 == i) {
-			if(threadIdx.y == 0)
-				top = sneighbors[threadIdx.x]; 
-			else
-				top = slattice[threadIdx.y - 1][threadIdx.x];
-			
-			if(threadIdx.x == 0)
-				left = sneighbors[3 * BLOCK_SIZE + threadIdx.y];
-			else
-				left = slattice[threadIdx.y][threadIdx.x - 1];
+		//Perform simulation
+		//Each turn of the loop performs BLOCK_SIZE^2 iterations of the Metropolis algorithm
+		for(int k = 0; k < iterations; k += (BLOCK_SIZE * BLOCK_SIZE)) {
+			for(int i = 0; i < 2; i ++) {
+				//Checkerboard
+				if((threadIdx.x + threadIdx.y) % 2 == i) {
+					if(threadIdx.y == 0)
+						top = sneighbors[threadIdx.x]; 
+					else
+						top = slattice[threadIdx.y - 1][threadIdx.x];
+					
+					if(threadIdx.x == 0)
+						left = sneighbors[3 * BLOCK_SIZE + threadIdx.y];
+					else
+						left = slattice[threadIdx.y][threadIdx.x - 1];
 
-			if(threadIdx.y == (BLOCK_SIZE - 1))
-				bottom = sneighbors[2 * BLOCK_SIZE + threadIdx.x]
-			else
-				bottom = slattice[threadIdx.y + 1][threadIdx.x];
+					if(threadIdx.y == (BLOCK_SIZE - 1))
+						bottom = sneighbors[2 * BLOCK_SIZE + threadIdx.x]
+					else
+						bottom = slattice[threadIdx.y + 1][threadIdx.x];
 
-			if(threadIdx.x == (BLOCK_SIZE - 1))
-				right = sneighbors[BLOCK_SIZE + threadIdx.y]; 
-			else
-				right = slattice[threadIdx.y][threadIdx.x + 1];
+					if(threadIdx.x == (BLOCK_SIZE - 1))
+						right = sneighbors[BLOCK_SIZE + threadIdx.y]; 
+					else
+						right = slattice[threadIdx.y][threadIdx.x + 1];
 
-			//Calculate change in energy if dipole were flipped
-			deltaU = 2 * slattice[threadIdx.y][threadIdx.x] * (top + bottom + left + right);
+					//Calculate change in energy if dipole were flipped
+					deltaU = 2 * slattice[threadIdx.y][threadIdx.x] * (top + bottom + left + right);
 
-			//If the energy would decrease, flip the dipole
-			if(deltaU <= 0)
-				slattice[threadIdx.y][threadIdx.x] *= -1;
-			else {
-				rand = curand_uniform(randState);
-				//TODO: Put in a more precise value of e
-				//Else the probability of a flip is given by the Boltzmann factor
-				if(rand < powf(2.71f, -deltaU/T))
-					slattice[threadIdx.y][threadIdx.x] *= -1;
+					//If the energy would decrease, flip the dipole
+					if(deltaU <= 0)
+						slattice[threadIdx.y][threadIdx.x] *= -1;
+					else {
+						rand = curand_uniform(randState);
+						//TODO: Put in a more precise value of e
+						//Else the probability of a flip is given by the Boltzmann factor
+						if(rand < powf(2.71f, -deltaU/T))
+							slattice[threadIdx.y][threadIdx.x] *= -1;
+					}
+					__syncthreads(); //Is this needed?
+				}
 			}
-			__syncthreads(); //Is this needed?
 		}
-	}
 	slattice[threadIdx.y][threadIdx.x] = lattice[ty * width + threadIdx.x];
 	}
 
